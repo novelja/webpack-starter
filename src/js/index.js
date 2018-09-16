ffeb0be238bb30f14c2f8a05d29ba14926333807
Novelja <novelja@gmail.com>
2018-09-16 12:18:01 +0200

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import style from '../scss/main.scss';
+
+let a = 'Novelja';
+let next = () => console.log(`My name is: ${a}`);
+next();
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: "./src/js/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+          // options: { minimize: true }
+        }
+      },
+      {
+        test: /\.scss/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(jpe?g|png)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            outputPath: "img/",
+            publicPath: "img/"
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chinkFilename: "[id].css"
+    })
+  ]
+};
